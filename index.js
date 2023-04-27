f922e1ac6d8d1461b5fd5aecc2923560ff9417f0
Md-arefin <ansariarefin617@gmail.com>
2023-04-27 13:29:05 +0600

    first commit

--- index.js
+++ index.js
@@ -17,6 +17,32 @@ app.get('/categories', (req, res) =>{
     res.send(categories);
 })
 
+app.get('/news', (req, res) =>{
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) =>{
+    const id = req.params.id;
+    // console.log(id);
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+
+app.get('/categories/:id', (req, res) =>{
+    const id = req.params.id;
+    // const id = parseInt(req.params.id)
+    // console.log(id);
+    if(id == 0){
+        res.send(news)
+    }
+    else{
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+        // news.filter(n => parseInt(n.category_id) === id);
+        // res.send(categoryNews)
+    }
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
